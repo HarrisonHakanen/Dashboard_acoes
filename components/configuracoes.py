a3c8b5d04a4cad43fb4f93891f06d48cd35a3446
HarrisonHakanen <42424389+HarrisonHakanen@users.noreply.github.com>
2022-11-05 16:03:19 -0300

    Commit

--- components/configuracoes.py
+++ components/configuracoes.py
@@ -22,6 +22,65 @@ import funcoes
 
 layout = dbc.Col([
 
-	html.Legend("Configurações")
+	html.Legend("Configurações"),
+
+	dbc.Row([
+		html.H5("Bollinger"),
+		dbc.Col([
+			
+			html.H5("Período de análise"),
+
+			dcc.DatePickerRange(
+				id="periodo_analise",
+				start_date_placeholder_text="Data inicial",
+				end_date_placeholder_text="Data final",   
+				calendar_orientation='vertical')
+		]),
+
+		dbc.Col([
+			html.H5("Dias anteriores (padrão 10)"),
+			dcc.Input(id="dias_anteriores", type="number",className="form-control form-control-lg"),
+		]),
+
+		dbc.Col([
+			html.H5("Quantidade de desvio padrão superior (padrão 1.5)"),
+			dcc.Input(id="mm_superior", type="number",className="form-control form-control-lg"),
+		]),
+		dbc.Col([
+			html.H5("Quantidade de desvio padrão inferior (padrão 1.5)"),
+			dcc.Input(id="mm_inferior", type="number",className="form-control form-control-lg"),
+		]),
+	],style={"padding": "25px"}),
+
+
+	dbc.Row([
+
+		html.H5("MACD"),
+
+		dbc.Col([
+			
+			html.H5("Período de análise"),
+
+			dcc.DatePickerRange(
+				id="periodo_analise_MACD",
+				start_date_placeholder_text="Data inicial",
+				end_date_placeholder_text="Data final",   
+				calendar_orientation='vertical')
+		]),
+
+		dbc.Col([
+			html.H5("Rápida MM (padrão 12)"),
+			dcc.Input(id="rapida_mm", type="number",className="form-control form-control-lg"),
+		]),
+
+		dbc.Col([
+			html.H5("Lenta MM (padrão 26)"),
+			dcc.Input(id="lenta_mm", type="number",className="form-control form-control-lg"),
+		]),
+		dbc.Col([
+			html.H5("Sinal (padrão 9)"),
+			dcc.Input(id="sinal", type="number",className="form-control form-control-lg"),
+		]),
+		],style={"padding": "25px"}),
 
 ])
